748717368d1e5967d5ef55396cae47c9cd67da35
Jonathan Nolis <jnolis@noreply.users.github.com>
2017-10-21 19:02:59 -0700

    Now saves an image to an S3 bucket

--- Program.fs
+++ Program.fs
@@ -1,9 +1,22 @@
-﻿open System
+﻿namespace PostcardLambda
+
+open System
+open System.IO
 open Newtonsoft.Json
 open Newtonsoft.Json.Linq
-open System.Text.RegularExpressions
 open System.Net.Http
 open SixLabors.ImageSharp
+open SixLabors.ImageSharp.Processing
+open Amazon.S3
+open Amazon.S3.Model
+open Amazon.Lambda.Core
+
+type MiniPhotoInfo = 
+    {
+        Id: int64
+        Secret: string
+        OriginalFormat: string
+    }
 
 type PhotoInfo = 
     {
@@ -19,23 +32,32 @@ type PhotoInfo =
         PublicUrl: string option
     }
 
-[<EntryPoint>]
-let main argv =
-    let apiKey = ""
-    
+module Program =
+
+
+    let apiKey =
+        "config.json"
+        |> File.ReadAllText
+        |> JObject.Parse
+        |> (fun x -> string x.["ApiKey"])
 
     let getPhoto (apiKey: string) (photoInfo: PhotoInfo) =
         let url = 
-            "https://api.flickr.com/services/rest/?method=flickr.photos.getInfo" + 
-            "&api_key=" + apiKey + 
-            "&photo_id=" + photoInfo.Id.ToString() +
-            "&secret=" + photoInfo.Secret +
-            "&format=json"
+            "https://farm" + photoInfo.Farm + 
+            ".staticflickr.com/" + photoInfo.Server + 
+            "/" + photoInfo.Id.ToString() + 
+            "_" + photoInfo.OriginalSecret + 
+            "_o." + photoInfo.OriginalFormat
         use client = new HttpClient()
-        client.GetByteArrayAsync(url)
-        |> Async.AwaitTask
-        |> Async.RunSynchronously
-        |> Image.Load        
+        use response = 
+            client.GetAsync(url)
+            |> Async.AwaitTask
+            |> Async.RunSynchronously
+        let stream =  
+            response.Content.ReadAsStreamAsync()
+            |> Async.AwaitTask
+            |> Async.RunSynchronously
+        Image.Load<Rgba32>(stream)  
 
     let getJson (url:string) = 
         let fixFlickrJson (s:string) = s.Substring(14,s.Length-15)
@@ -87,37 +109,100 @@ let main argv =
         }
 
     let getPhotos (apiKey) (tags: string list) =
-        let url = 
-            "https://api.flickr.com/services/rest/?method=flickr.photos.search" +
-            "&api_key=" + apiKey + 
-            "&tags=" + (List.reduce (fun x y -> x + "," + y) tags) +  
-            "&sort=relevance&content_type=1&license=2&format=json&per_page=10"
-        getJson(url)
-        |> JObject.Parse
-        |> (fun j -> j.["photos"].["photo"])
-        |> Seq.map (fun p -> (p.["id"].ToObject<int64>(),p.["secret"].ToObject<string>()))
-        |> List.ofSeq
+        let getPhotoPage (apiKey) (tags: string list) (perPage: int) (page: int option) =
+            let url = 
+                "https://api.flickr.com/services/rest/?method=flickr.photos.search" +
+                "&api_key=" + apiKey + 
+                "&tags=" + (List.reduce (fun x y -> x + "," + y) tags) +  
+                "&sort=relevance&content_type=1&license=2&format=json" + 
+                "&extras=original_format" + 
+                "&per_page=" + perPage.ToString() +  
+                "&page=" + (match page with | Some p -> p.ToString() | None -> "1")
+            getJson(url)
+            |> JObject.Parse
+            |> (fun j -> j.["photos"].["photo"])
+            |> Seq.map (fun p -> {
+                                    MiniPhotoInfo.Id = p.["id"].ToObject<int64>();
+                                    MiniPhotoInfo.Secret = p.["secret"].ToObject<string>();
+                                    MiniPhotoInfo.OriginalFormat = p.["originalformat"].ToObject<string>();   
+                                })
+            |> List.ofSeq
 
+        let getPhotoPageCount (apiKey) (tags: string list) (perPage: int) =
+            let url = 
+                "https://api.flickr.com/services/rest/?method=flickr.photos.search" +
+                "&api_key=" + apiKey + 
+                "&tags=" + (List.reduce (fun x y -> x + "," + y) tags) +  
+                "&sort=relevance&content_type=1&license=2&format=json" + 
+                "&per_page=" + perPage.ToString()
+            getJson(url)
+            |> JObject.Parse
+            |> (fun j -> j.["photos"].["pages"].ToObject<int>())
         
-    let isGoodSizePhoto (width:int) (height:int) = 
-        width >= 1875 &&
-        height >= 1275 &&
+        let perPage = 25
+        let pageCount = getPhotoPageCount apiKey tags perPage
+        let page = System.Random().Next(max 1 pageCount/4)
+        System.Console.WriteLine (page.ToString())
+        getPhotoPage apiKey tags perPage (Some page)
+        
+        
+    let isGoodSizePhoto desiredWidth desiredHeight (width:int) (height:int) = 
+        width >= desiredWidth &&
+        height >= desiredHeight &&
         ((float width)/(float height) > 1.4) && 
         ((float width)/(float height) < 1.6)
 
-    let photo =
-        getPhotos apiKey (List.singleton "flowers")
-        |> Array.ofList
-        |> Array.Parallel.choose (fun (i, s) -> 
-              let size = getPhotoSize apiKey i
-              match size with
-              | Some (w,h) when isGoodSizePhoto w h -> Some (i,s)
-              | _ -> None)
-        |> List.ofArray
-        |> (fun l -> List.item (System.Random().Next l.Length) l)
-        |> (fun (i,s) -> getPhotoInfo apiKey i s)
-        |> (fun p -> getPhoto apiKey p)
-    photo.
+    let getGoodPhotos (tries: int) (apiKey: string) (tags: string list) desiredWidth desiredHeight= 
+        let getGoodPhotoAttempt apiKey tags =
+            getPhotos apiKey tags
+            |> Array.ofList
+            |> Array.Parallel.choose (fun mp -> 
+                  let size = getPhotoSize apiKey mp.Id
+                  let format = mp.OriginalFormat
+                  match (size,format) with
+                  | (Some (w,h),f) when isGoodSizePhoto desiredWidth desiredHeight w h && f = "jpg"-> Some mp
+                  | _ -> None)         
+            |> List.ofArray
+        Seq.init tries (fun i -> getGoodPhotoAttempt apiKey tags)
+        |> Seq.skipWhile List.isEmpty
+        |> Seq.tryHead
+        |> (fun x -> match x with | Some y -> y | None -> List.empty)
 
-        
-    0 // return an integer exit code
+    let resize (width: int) (height: int) (p:Image<Rgba32>) =
+        let ratio = max ((float width)/(float p.Width)) ((float height)/(float p.Height))
+        let adjust x = x |> float |> (*) ratio |> ceil |> int
+        System.Console.WriteLine (p.Width.ToString() + "x" + p.Height.ToString() + " -> " + (adjust p.Width).ToString() + "x" + (adjust p.Height).ToString()) 
+        p.Mutate(fun x -> x.Resize(SixLabors.Primitives.Size(adjust p.Width,adjust p.Height), Lanczos5Resampler(), false) |> ignore)
+        p.Mutate(fun x -> x.Crop(SixLabors.Primitives.Rectangle(0,0, width, height)) |> ignore)
+        p
+
+    let desiredWidth = 1875
+    let desiredHeight = 1275        
+
+    let photo =
+        getGoodPhotos 5 apiKey (List.singleton "flowers") desiredWidth desiredHeight
+        |> (fun x->
+            if List.isEmpty x then failwith "No pictures found :("
+            else x
+            )
+        |> (fun l -> List.item (System.Random().Next l.Length) l)
+        |> (fun mp -> getPhotoInfo apiKey mp.Id mp.Secret)
+        |> (fun p -> getPhoto apiKey p)
+        |> resize desiredWidth desiredHeight
+
+    let handler(context:ILambdaContext) =
+        let memoryStream = new MemoryStream()
+        photo.SaveAsJpeg(memoryStream)
+        let client = new AmazonS3Client(Amazon.RegionEndpoint.USWest2)
+        let putRequest = PutObjectRequest(BucketName="postcard-lambda",Key = System.Guid.NewGuid().ToString(),ContentType="image/jpeg",InputStream=memoryStream)
+        client.PutObjectAsync putRequest
+        |> Async.AwaitTask
+        |> Async.RunSynchronously
+        |> ignore
+
+    [<EntryPointAttribute>]
+    let main args =
+        let fileStream = new FileStream(args.[0],FileMode.Create)
+        photo.SaveAsJpeg(fileStream)
+        fileStream.Dispose()
+        0
